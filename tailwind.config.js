40a39dfd68e8e424ab8600b8182d14a20e2c4e23
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-07-29 17:10:10 -0600

    Update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
+    colors: {
+      technicolor: {
+        blue: 'var(--technicolor-blue)',
+        orange: 'var(--technicolor-orange)',
+        plum: 'var(--technicolor-plum)',
+        pavement: 'var(--technicolor-pavement)',
+        green: 'var(--technicolor-green)',
+        yellow: 'var(--technicolor-yellow)',
+      },
+      white: '#ECECE3',
+    },
+    variables: {
+      DEFAULT: {
         technicolor: {
-          blue: '##008CD1',
+          blue: '#008CD1',
           orange: '#F36523',
           plum: '#942977',
           pavement: '#161616',
           green: '#008A4C',
           yellow: '#FFC20F',
         },
-        white: '#ECECE3',
       },
     },
   },
@@ -23,5 +33,8 @@ export default {
     require('@tailwindcss/aspect-ratio'),
     // eslint-disable-next-line no-undef
     require('@tailwindcss/typography'),
+    // eslint-disable-next-line no-undef
+    require('@mertasan/tailwindcss-variables'),
   ],
+  darkMode: ['selector', '[data-mode="dark"]'],
 };
