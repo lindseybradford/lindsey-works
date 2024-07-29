e3a0e3e7a4a43369417094cae1f4b45ee5ab06ad
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-07-29 14:35:51 -0600

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        technicolor: {
+          blue: '##008CD1',
+          orange: '#F36523',
+          plum: '#942977',
+          pavement: '#161616',
+          green: '#008A4C',
+          yellow: '#FFC20F',
+        },
+        white: '#ECECE3',
+      },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/typography'),
-    ],
-}
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('@tailwindcss/forms'),
+    // eslint-disable-next-line no-undef
+    require('@tailwindcss/aspect-ratio'),
+    // eslint-disable-next-line no-undef
+    require('@tailwindcss/typography'),
+  ],
+};
