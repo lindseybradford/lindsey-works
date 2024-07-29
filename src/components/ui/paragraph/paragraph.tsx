209fdb5d6d2b522db68abf7938191db4b39bb82e
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-07-29 17:09:13 -0600

    Update Paragraph

--- src/components/ui/paragraph/paragraph.tsx
+++ src/components/ui/paragraph/paragraph.tsx
@@ -1,18 +1,35 @@
+import { createElement } from 'react';
+
 import { cn } from '@src/utils/cn';
 
+export enum ParagraphVariant {
+  Base = 'base',
+  Small = 'small',
+  Large = 'large',
+}
+
 type ParagraphProps = {
+  variant?: ParagraphVariant;
+  isFlush?: boolean;
   className?: string;
   children: React.ReactNode;
 };
 
 export const Paragraph = ({
+  variant = ParagraphVariant.Base,
+  isFlush = false,
   className,
   children,
-  ...props
 }: ParagraphProps) => {
-  return (
-    <p className={cn('', className)} {...props}>
-      {children}
-    </p>
+  const ParagraphStyles = new Map([
+    [ParagraphVariant.Base, `text-base leading-relaxed ${!isFlush && 'mb-10'}`],
+  ]);
+
+  return createElement(
+    'p',
+    {
+      className: cn(ParagraphStyles.get(variant), className),
+    },
+    children
   );
 };
