672b5123a5e1fe0e9fd7945ddc92045b74813116
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-07-29 17:09:05 -0600

    Update MetaLabel

--- src/components/ui/meta-label/meta-label.tsx
+++ src/components/ui/meta-label/meta-label.tsx
@@ -1,18 +1,19 @@
+import { createElement } from 'react';
 import { cn } from '@src/utils/cn';
 
 type MetaLabelProps = {
+  tagName: `h3` | `h4` | `h5` | `h6` | `p`;
   className?: string;
   children: React.ReactNode;
 };
 
-export const MetaLabel = ({
-  className,
-  children,
-  ...props
-}: MetaLabelProps) => {
-  return (
-    <h3 className={cn('', className)} {...props}>
-      {children}
-    </h3>
+export const MetaLabel = ({ tagName, className, children }: MetaLabelProps) => {
+  return createElement(
+    tagName,
+    {
+      className: cn('flex items-center m-0 text-xs mt-2', className),
+    },
+    createElement('div', { className: `h-2 w-2 mr-2 bg-[--group-color]` }),
+    children
   );
 };
