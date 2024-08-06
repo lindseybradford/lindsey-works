511fca4cc28f1d45eb08ada98698e0a29ffa43b5
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-08-06 13:12:46 -0600

    Add react-lazy-load and vite-imagetools

--- src/components/ui/image/image.tsx
+++ src/components/ui/image/image.tsx
@@ -1,15 +1,32 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 import { cn } from '@src/utils/cn';
 
 type ImageProps = {
   src: string;
+  height: string;
+  width: string;
   alt: string;
   className?: string;
   classNameInner?: string;
 };
-export const Image = ({ src, alt, className, classNameInner }: ImageProps) => {
+export const Image = ({
+  src,
+  height,
+  width,
+  alt,
+  className,
+  classNameInner,
+}: ImageProps) => {
   return (
     <div className={cn(className, 'relative')}>
-      <img src={src} alt={alt} className={cn(classNameInner, 'absolute')} />
+      <LazyLoadImage
+        src={src}
+        height={height}
+        width={width}
+        alt={alt}
+        className={cn(classNameInner, 'absolute')}
+      />
     </div>
   );
 };
