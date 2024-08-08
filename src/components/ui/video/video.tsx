7bd3283d5eeafd459b957d0073d8a2d81aa60216
Lindsey Bradford <lindsey.bradford@mixpanel.com>
2024-08-08 14:16:53 -0600

    Add Video

---
+++ src/components/ui/video/video.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { MediaAnimation } from '@src/constants';
+import { cn } from '@src/utils/cn';
+
+import './video.css';
+
+type VideoProps = {
+  mp4Src: string;
+  webmSrc: string;
+  height: string;
+  width: string;
+  className?: string;
+  isRounded?: boolean;
+  playAsGif?: boolean;
+  animationOnLoad?: MediaAnimation;
+};
+
+export const Video = ({
+  isRounded = true,
+  animationOnLoad = MediaAnimation.Fade,
+  ...props
+}: VideoProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const useAnimation = animationOnLoad !== MediaAnimation.None;
+  const useFadeAnimation = animationOnLoad === MediaAnimation.Fade;
+
+  const videoAttributes = {
+    ...(props.playAsGif && {
+      autoPlay: true,
+      muted: true,
+      loop: true,
+      playsInline: true,
+    }),
+  };
+
+  useEffect(() => {
+    videoRef.current!.onloadeddata = () => {
+      videoRef.current!.readyState >= HTMLMediaElement.HAVE_CURRENT_DATA &&
+        setIsLoaded(true);
+    };
+  });
+
+  return (
+    <video
+      className={cn(
+        props.className,
+        'relative',
+        isRounded && 'rounded-3xl',
+        useAnimation && 'transition-opacity duration-500',
+        !isLoaded && useFadeAnimation && 'opacity-0',
+        isLoaded && useFadeAnimation && 'opacity-1'
+      )}
+      ref={videoRef}
+      height={props.height}
+      width={props.width}
+      {...videoAttributes}
+    >
+      <source src={props.mp4Src} type="video/webm" />
+      <source src={props.webmSrc} type="video/mp4" />
+    </video>
+  );
+};
